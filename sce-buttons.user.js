8a68425aab5d7a57ebdb67c32fb75588a16aacea
SychO <SychO9@users.noreply.github.com>
2018-04-06 15:11:24 +0200

    Update sce-buttons.user.js

--- sce-buttons.user.js
+++ sce-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Card Exchange Buttons
 // @namespace    https://steamcommunity.com/id/SychO_Droid
-// @updateURL    https://cdn.rawgit.com/SychO9/Steam-Card-Exchange-Buttons/master/sce-buttons.user.js
+// @updateURL    https://rawgit.com/SychO9/Steam-Card-Exchange-Buttons/master/sce-buttons.user.js
 // @version      1.5
 // @description  Fast Link To SCE Badge Page From Badge Progress Page.
 // @author       SychO
